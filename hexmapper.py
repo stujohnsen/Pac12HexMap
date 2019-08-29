30a917b77a21f03ac80c7a85fb74590aee268fe0
Stuart Johnsen <stujohnsen@gmail.com>
2019-08-29 16:04:08 -0600

    working on map builder

--- hexmapper.py
+++ hexmapper.py
@@ -1,18 +1,4 @@
 import time
-# from tables import *
-
-# # neighboring indices are numerated clockwise from 12 o clock position
-# class HexTile:
-#     index       = Int16Col()
-#     x_coord     = Float16Col()
-#     y_coord     = Float16Col()
-#     control     = StringCol()
-#     n_top       = Int16Col()
-#     n_top_right = Int16Col()
-#     n_bot_right = Int16Col()
-#     n_bot       = Int16Col()
-#     n_bot_left  = Int16Col()
-#     n_top_left  = Int16Col()
 
 if __name__ == "__main__":
     inCoordFile = open("coordmap", "r")
@@ -104,35 +90,35 @@ if __name__ == "__main__":
 
     svgFileContents += \
         '\t<defs> \n' + \
-            '\t\t<style>\n' + \
-            '\t\t\tpolygon {stroke: #000; stroke-width: ' + repr(strokeWidth) + 'px;}\n' + \
-            '\t\t\tpolygon[control="base"]             {fill:grey}\n' + \
-            '\t\t\tpolygon[control="baseborder"]       {fill:black}\n' + \
-            '\t\t\tpolygon[control="Arizona"]          {fill:#CC0033}\n' + \
-            '\t\t\tpolygon[control="ArizonaBorder"]    {fill:#003366}\n' + \
-            '\t\t\tpolygon[control="ASU"]              {fill:#FFC425}\n' + \
-            '\t\t\tpolygon[control="ASUBorder"]        {fill:#A40046}\n' + \
-            '\t\t\tpolygon[control="Cal"]              {fill:#041E42}\n' + \
-            '\t\t\tpolygon[control="CalBorder"]        {fill:#FFC72C}\n' + \
-            '\t\t\tpolygon[control="Colorado"]         {fill:#CFB87C}\n' + \
-            '\t\t\tpolygon[control="ColoradoBorder"]   {fill:#565A5C}\n' + \
-            '\t\t\tpolygon[control="Oregon"]           {fill:#124734}\n' + \
-            '\t\t\tpolygon[control="OregonBorder"]     {fill:#FEE123}\n' + \
-            '\t\t\tpolygon[control="OSU"]              {fill:#D85A1A}\n' + \
-            '\t\t\tpolygon[control="OSUBorder"]        {fill:#685040}\n' + \
-            '\t\t\tpolygon[control="Stanford"]         {fill:#990000}\n' + \
-            '\t\t\tpolygon[control="StanfordBorder"]   {fill:#175E54}\n' + \
-            '\t\t\tpolygon[control="UCLA"]             {fill:#3284BF}\n' + \
-            '\t\t\tpolygon[control="UCLABorder"]       {fill:#FFE800}\n' + \
-            '\t\t\tpolygon[control="USC"]              {fill:#991B1E}\n' + \
-            '\t\t\tpolygon[control="USCBorder"]        {fill:#FFCC00}\n' + \
-            '\t\t\tpolygon[control="Utah"]             {fill:#CC0000}\n' + \
-            '\t\t\tpolygon[control="UtahBorder"]       {fill:#808080}\n' + \
-            '\t\t\tpolygon[control="Washington"]       {fill:#33006F}\n' + \
-            '\t\t\tpolygon[control="WashingtonBorder"] {fill:#E8D3A2}\n' + \
-            '\t\t\tpolygon[control="WSU"]              {fill:#981E32}\n' + \
-            '\t\t\tpolygon[control="WSUBorder"]        {fill:#53565A}\n' + \
-            '\t\t</style>\n' + \
+        '\t\t<style>\n' + \
+        '\t\t\tpolygon {stroke: #000; stroke-width: ' + repr(strokeWidth) + 'px;}\n' + \
+        '\t\t\tpolygon[control="base"]             {fill:grey}\n' + \
+        '\t\t\tpolygon[control="baseborder"]       {fill:black}\n' + \
+        '\t\t\tpolygon[control="Arizona"]          {fill:#CC0033}\n' + \
+        '\t\t\tpolygon[control="ArizonaBorder"]    {fill:#003366}\n' + \
+        '\t\t\tpolygon[control="ASU"]              {fill:#FFC425}\n' + \
+        '\t\t\tpolygon[control="ASUBorder"]        {fill:#A40046}\n' + \
+        '\t\t\tpolygon[control="Cal"]              {fill:#041E42}\n' + \
+        '\t\t\tpolygon[control="CalBorder"]        {fill:#FFC72C}\n' + \
+        '\t\t\tpolygon[control="Colorado"]         {fill:#CFB87C}\n' + \
+        '\t\t\tpolygon[control="ColoradoBorder"]   {fill:#565A5C}\n' + \
+        '\t\t\tpolygon[control="Oregon"]           {fill:#124734}\n' + \
+        '\t\t\tpolygon[control="OregonBorder"]     {fill:#FEE123}\n' + \
+        '\t\t\tpolygon[control="OSU"]              {fill:#D85A1A}\n' + \
+        '\t\t\tpolygon[control="OSUBorder"]        {fill:#685040}\n' + \
+        '\t\t\tpolygon[control="Stanford"]         {fill:#990000}\n' + \
+        '\t\t\tpolygon[control="StanfordBorder"]   {fill:#175E54}\n' + \
+        '\t\t\tpolygon[control="UCLA"]             {fill:#3284BF}\n' + \
+        '\t\t\tpolygon[control="UCLABorder"]       {fill:#FFE800}\n' + \
+        '\t\t\tpolygon[control="USC"]              {fill:#991B1E}\n' + \
+        '\t\t\tpolygon[control="USCBorder"]        {fill:#FFCC00}\n' + \
+        '\t\t\tpolygon[control="Utah"]             {fill:#CC0000}\n' + \
+        '\t\t\tpolygon[control="UtahBorder"]       {fill:#808080}\n' + \
+        '\t\t\tpolygon[control="Washington"]       {fill:#33006F}\n' + \
+        '\t\t\tpolygon[control="WashingtonBorder"] {fill:#E8D3A2}\n' + \
+        '\t\t\tpolygon[control="WSU"]              {fill:#981E32}\n' + \
+        '\t\t\tpolygon[control="WSUBorder"]        {fill:#53565A}\n' + \
+        '\t\t</style>\n' + \
         '\t</defs>\n'
 
     for hex in hexGrid:
@@ -142,4 +128,3 @@ if __name__ == "__main__":
     svgFileName = 'hexMap' + time.strftime("%Y%m%d-%H%M%S") + '.svg'
     outSVGFile = open(svgFileName, "w")
     outSVGFile.write(svgFileContents)
-
--- mapcolorer.py
+++ mapcolorer.py
@@ -1,78 +1,205 @@
 import time, json
+from fractions import Fraction
+from collections import defaultdict
 # import cairosvg
 import xml.etree.ElementTree as ET
 
-if __name__ == "__main__":
+class coord:
+    row = int()
+    col = int()
 
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return (self.row == other.row) and (self.col == other.col)
+        else:
+            return False
+
+    def __hash__(self):
+        return hash((self.row, self.col))
+
+class neighbors:
+    top_left     = coord(None, None)
+    top_right    = coord(None, None)
+    right        = coord(None, None)
+    bottom_right = coord(None, None)
+    bottom_left  = coord(None, None)
+    left         = coord(None, None)
+
+def SetNeighborTile(controlDict, tile, teamToSet):
+    if tile.
+
+    if any neighbor to input tile belongs to other team or any neighbor to input file not in controlDict
+        set tile control color to team border
+    else
+        set tile control color to team
+    
+def UpdateTeamNeighborSet(inControlDict, team):
+    startingSet = filter inControlDict by team as set
+    neighborsSet = GetAllUnsetNeighbors(startingSet)
+    if neighborsSet is empty:
+        return GetAllUnsetNeighbors(neighborsSet)
+    else:
+        return neighborsSet
+
+def GetAllUnsetNeighbors(inSetToExpand):
+    return unset neighbors of inSetToExpand as set
+
+def SelectTileFromSet(teamNeighborSet):
+    while len(teamNeighborSet) > 0:
+        selectedTile = select random tile from teamNeighborSet
+        remove selectedTile from teamNeighborSet
+        if selectedTile is unset
+            return selected tile
+    return None
+
+def BuildMapForGroup(inTileGroupControlDict, inTileGroupNeighborsDict, inTeamWeights):
+    lTotalUnallocatedTiles = len(inTileGroupControlDict)
+
+    lRemainingTeamTiles = dict()
+    for team, weight in inTeamWeights.keys():
+        lRemainingTeamTiles[team] = int(lTotalTiles * weight)
+
+    while(lTotalUnallocatedTiles > 0):
+        for team, teamTilesToAllocate in lRemainingTeamTiles:
+            if(teamTilesToAllocate <= 0 or lTotalUnallocatedTiles <= 0)
+                continue
+            while True:
+                if len(teamNeighborSet) <= 0
+                    teamNeighborSet = UpdateTeamNeighborSet(inTileGroupControlDict, team)
+                lTile = SelectTileFromSet(teamNeighborSet)
+                if lTile = None
+                    continue
+                SetNeighborTile(controlDict, tile, teamToSet)
+                break
+
+            teamTilesToAllocate -= 1
+            lTotalUnallocatedTiles -= 1
+            
+
+
+
+    return
+
+
+
+if __name__ == "__main__":
+    
     inStartTilesFile = open('starttiles.json', 'r')
     inStartCoordsJson = inStartTilesFile.read()
-    lStartCoordsDict = json.loads(inStartCoordsJson)
-    Cal        = lStartCoordsDict['North']['Cal']
-    Oregon     = lStartCoordsDict['North']['Oregon']
-    OSU        = lStartCoordsDict['North']['OSU']
-    Stanford   = lStartCoordsDict['North']['Stanford']
-    Washington = lStartCoordsDict['North']['Washington']
-    WSU        = lStartCoordsDict['North']['WSU']
-    Arizona    = lStartCoordsDict['South']['Arizona']
-    ASU        = lStartCoordsDict['South']['ASU']
-    Colorado   = lStartCoordsDict['South']['Colorado']
-    UCLA       = lStartCoordsDict['South']['UCLA']
-    USC        = lStartCoordsDict['South']['USC']
-    Utah       = lStartCoordsDict['South']['Utah']
-    
+    lTeamsInfoDict = json.loads(inStartCoordsJson)
+
     ET.register_namespace('', "http://www.w3.org/2000/svg")
     ET.register_namespace('xmls:xlink', "http://www.w3.org/1999/xlink")
-
-
-    tree = ET.parse('JS Basemap.svg')
+    tree = ET.parse('Basemap.svg')
     root = tree.getroot()
 
+    lNorthTeamsInfoDict = lTeamsInfoDict['North'].items()
+    lNorthTeamsStartCoords = dict()
+    lNorthTeamWeights = dict()
+    for key, value in lNorthTeamsInfoDict:
+        lNorthTeamsStartCoords[key] = coord(value['row'], value['col'])
+        weight = value['weight']
+        if '/' in weight:
+            fraction = Fraction(weight)
+            lNorthTeamWeights[key] = float(fraction)
+        else:
+            lNorthTeamWeights[key] = weight
+
+    
     NorthGroup = tree.findall(".//{http://www.w3.org/2000/svg}g[@id='North']//")
-    SouthGroup = tree.findall(".//{http://www.w3.org/2000/svg}g[@id='South']//")
+
+    # northTileList = list()
+    lNorthTileNeighborsDict = defaultdict(coord)
+    lNorthTileControlDict = defaultdict(coord)
 
     for tile in NorthGroup:
         row = int(tile.attrib.get('row'))
         col = int(tile.attrib.get('col'))
+        tileCoord = coord(row, col)
+        tileNeighbors = neighbors()
+        
+        if(row % 2 == 0):
+            tileNeighbors.top_left     = coord(row - 1, col - 1)
+            tileNeighbors.top_right    = coord(row - 1, col    )
+            tileNeighbors.right        = coord(row    , col + 1)
+            tileNeighbors.bottom_right = coord(row + 1, col    )
+            tileNeighbors.bottom_left  = coord(row + 1, col - 1)
+            tileNeighbors.left         = coord(row    , col - 1)
+        else:
+            tileNeighbors.top_left     = coord(row - 1, col    )
+            tileNeighbors.top_right    = coord(row - 1, col + 1)
+            tileNeighbors.right        = coord(row    , col + 1)
+            tileNeighbors.bottom_right = coord(row + 1, col + 1)
+            tileNeighbors.bottom_left  = coord(row + 1, col    )
+            tileNeighbors.left         = coord(row    , col - 1)
 
-        if   (row == Cal['row']) and (col == Cal['col']) :
-            tile.set('control', 'Cal')
-        elif (row == Oregon['row']) and (col == Oregon['col']) :
-            tile.set('control', 'Oregon')
-        elif (row == OSU['row']) and (col == OSU['col']) :
-            tile.set('control', 'OSU')
-        elif (row == Stanford['row']) and (col == Stanford['col']) :
-            tile.set('control', 'Stanford')
-        elif (row == Washington['row']) and (col == Washington['col']) :
-            tile.set('control', 'Washington')
-        elif (row == WSU['row']) and (col == WSU['col']) :
-            tile.set('control', 'WSU')
+        lNorthTileNeighborsDict[tileCoord] = tileNeighbors
 
-    SouthGroupTemp = dict();
+        if tileCoord in lNorthTeamsStartCoords.values():
+            for key, value in lNorthTeamsStartCoords.items():
+                if(value == tileCoord and lNorthTeamWeights[key] > 0):
+                    control = key
+        else:
+            control = tile.attrib.get('control')
 
-    for tile in SouthGroup:
-        row = int(tile.attrib.get('row'))
-        col = int(tile.attrib.get('col'))
-        XCoord = int(tile.attrib.get('YCoord'))
-        YCoord = int(tile.attrib.get('XCoord'))
-        control = int(tile.attrib.get('control'))
+        lNorthTileControlDict[tileCoord] = control
 
-        if   (row == Arizona['row']) and (col == Arizona['col']) :
-            tile.set('control', 'Arizona')
-        elif (row == ASU['row']) and (col == ASU['col']) :
-            tile.set('control', 'ASU')
-        elif (row == Colorado['row']) and (col == Colorado['col']) :
-            tile.set('control', 'Colorado')
-        elif (row == UCLA['row']) and (col == UCLA['col']) :
-            tile.set('control', 'UCLA')
-        elif (row == USC['row']) and (col == USC['col']) :
-            tile.set('control', 'USC')
-        elif (row == Utah['row']) and (col == Utah['col']) :
-            tile.set('control', 'Utah')
 
-        SouthGroupTemp[row][col][]
+    BuildMapForGroup(lNorthTileControlDict, lNorthTileNeighborsDict, lNorthTeamWeights)
+
+        # startTiles = list(filter(lambda x: x.values() != 'base', northTileControlDict))
+    # northStartTiles = {key:value for key,value in northTileControlDict.items() if value != 'base'}
+
+    SouthGroup = tree.findall(".//{http://www.w3.org/2000/svg}g[@id='South']//")
+
+
+    # for tile in northTileList:
+
+
+
+    #     if   (row == Cal['row']) and (col == Cal['col']) :
+    #         tile.set('control', 'Cal')
+    #     elif (row == Oregon['row']) and (col == Oregon['col']) :
+    #         tile.set('control', 'Oregon')
+    #     elif (row == OSU['row']) and (col == OSU['col']) :
+    #         tile.set('control', 'OSU')
+    #     elif (row == Stanford['row']) and (col == Stanford['col']) :
+    #         tile.set('control', 'Stanford')
+    #     elif (row == Washington['row']) and (col == Washington['col']) :
+    #         tile.set('control', 'Washington')
+    #     elif (row == WSU['row']) and (col == WSU['col']) :
+    #         tile.set('control', 'WSU')
+
+    # SouthGroupTemp = dict();
+
+    # for tile in SouthGroup:
+    #     row = int(tile.attrib.get('row'))
+    #     col = int(tile.attrib.get('col'))
+    #     XCoord = int(tile.attrib.get('YCoord'))
+    #     YCoord = int(tile.attrib.get('XCoord'))
+    #     control = int(tile.attrib.get('control'))
+
+    #     if   (row == Arizona['row']) and (col == Arizona['col']) :
+    #         tile.set('control', 'Arizona')
+    #     elif (row == ASU['row']) and (col == ASU['col']) :
+    #         tile.set('control', 'ASU')
+    #     elif (row == Colorado['row']) and (col == Colorado['col']) :
+    #         tile.set('control', 'Colorado')
+    #     elif (row == UCLA['row']) and (col == UCLA['col']) :
+    #         tile.set('control', 'UCLA')
+    #     elif (row == USC['row']) and (col == USC['col']) :
+    #         tile.set('control', 'USC')
+    #     elif (row == Utah['row']) and (col == Utah['col']) :
+    #         tile.set('control', 'Utah')
+
+    #     SouthGroupTemp[row][col][]
 
     
-tree.write('output_test.svg')
+    # tree.write('output_test.svg')
 
 # cairosvg.svg2png(url="giftest/svg/output_michigan.svg", write_to="giftest/png/output_michigan.png")
 
